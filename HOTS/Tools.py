d41e4314b64f8e836158c04b998001c2c0ffc1f1
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-11-04 13:13:48 +0100

    Update Tools.py
    clean-up

--- HOTS/Tools.py
+++ HOTS/Tools.py
@@ -87,9 +87,33 @@ def predictioncosine(to_predict, prototype, homeo, R):
             output_distance[idx] = np.amax(Euclidian_distance)
     return output_distance, polarity.astype(int)
 
+#####################
+def GenerateHistogram(event):
+    '''
+    Generate an histogram for each sample (that is, between two changes).
+
+    INPUT :
+        + event (<object event>) stream on event on which we want to create an histogram
+    OUTPUT :
+        + freq = (<np.array>) of size (nb_samples,nb_clusters) representing the histrogram of cluster
+            activation for each sample
+        + pola = (<np.array>) of size (nb_sample,nb_clusters) representing the index of cluster activation
+    '''
+    last_change = 0
+    for idx, new_change in enumerate(event.ChangeIdx):
+        freq, polarities = np.histogram(
+            event.polarity[last_change:new_change+1], bins=len(event.ListPolarities))
+        if idx != 0:
+            freq_mat = np.vstack((freq_mat, freq))
+        else:
+            freq_mat = freq
+        last_change = new_change
+    return freq_mat, polarities
+
+
 def Norm(Hist, Histo_proto, method):
     '''
-    One function to pack all the norm
+    One function to pack all the hitstogram norms
     INPUT :
         + Hist : (<np.array>) matrix of size (nb_sample,nb_polarity) representing the histogram for each sample
         + Histo_proto : (<np.array>) matrix of size (nb_cluster,nb_polarity) representing the histogram for each
@@ -132,27 +156,3 @@ def LoadObject(filename):
     with open(filename, 'rb') as file:
         Clust = pickle.load(file)
     return Clust
-
-
-#####################
-def GenerateHistogram(event):
-    '''
-    Generate an histogram for each sample (that is, between two changes).
-
-    INPUT :
-        + event (<object event>) stream on event on which we want to create an histogram
-    OUTPUT :
-        + freq = (<np.array>) of size (nb_samples,nb_clusters) representing the histrogram of cluster
-            activation for each sample
-        + pola = (<np.array>) of size (nb_sample,nb_clusters) representing the index of cluster activation
-    '''
-    last_change = 0
-    for idx, new_change in enumerate(event.ChangeIdx):
-        freq, polarities = np.histogram(
-            event.polarity[last_change:new_change+1], bins=len(event.ListPolarities))
-        if idx != 0:
-            freq_mat = np.vstack((freq_mat, freq))
-        else:
-            freq_mat = freq
-        last_change = new_change
-    return freq_mat, polarities
